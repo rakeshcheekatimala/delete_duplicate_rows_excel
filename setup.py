49c75e135585f56f4ce2e7d67428915e9c7b40ec
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-12-03 18:22:33 +0800

    rollback

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.7",
     ],
     packages=["delete_duplicate_rows_excel"],
